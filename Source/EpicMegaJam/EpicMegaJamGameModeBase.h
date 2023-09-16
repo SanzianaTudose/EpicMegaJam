cff710a70764dc21ccaf38ffe33ebbe84cee830c
SanzianaTudose <tudosesanziana@gmail.com>
2023-09-16 15:53:12 +0200

    Initial commit

---
+++ Source/EpicMegaJam/EpicMegaJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EpicMegaJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICMEGAJAM_API AEpicMegaJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
