f09b26e4fdd35b12b8432e454c1f0dbef0f0bb6f
Willem Ploegstra <72265671+Willem-P1@users.noreply.github.com>
2023-09-18 19:33:59 +0200

    Scoring update (#9)
    Right now pressing Q will calculate a score between 0 and 10
    Reference to the players blocks is found on awake
    
    ---------
    
    Co-authored-by: SanzianaTudose <tudosesanziana@gmail.com>

--- Source/EpicMegaJam/ScoringComponent.cpp
+++ Source/EpicMegaJam/ScoringComponent.cpp
@@ -33,17 +33,44 @@ void UScoringComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 }
 
 
-int UScoringComponent::ScoreBlocks(TArray<int32> exampleBlocks, TArray<int32> userBlocks)
+float UScoringComponent::ScoreBlocks(TArray<int32> exampleBlocks, TArray<int32> userBlocks)
 {
-	int error = 0;
+	float error = 0;
+	int examplePrevPos = -400, userPrevPos = 1100;
 
-	for (int32 i = 0; i != exampleBlocks.Num(); ++i)
+	for (int32 i = 0; i != userBlocks.Num(); ++i)
 	{
-		int diff = exampleBlocks[i];// - userBlocks[i];
-		diff *= diff;
+		//get difference to previous position
+		int exampleDelta = exampleBlocks[i] - examplePrevPos;
+		int userDelta = userBlocks[i] - userPrevPos;
+
+		//update previous position
+		examplePrevPos = exampleBlocks[i];
+		userPrevPos = userBlocks[i];
+
+		//calculate difference between towers
+		float diff = exampleDelta - userDelta;
+		diff /= PenaltyPerUnit;
+		//calculate absolute error
+
+		if(diff < 0)
+		{
+			diff = -diff;
+		}
+		UE_LOG(LogTemp, Warning, TEXT("%f"), diff);
+
 		error += diff;
+	}
+	error /= userBlocks.Num();//calculate average error
+	error = 10 - error;//calculate score out of 10
+	if(error < 0)
+	{
+		error = 0;
 	}
 
 	return error;
+	// return exampleBlocks[0] + 400;
+	// return userBlocks[0] - 1100;
+
 }
 
